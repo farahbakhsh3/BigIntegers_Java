062b5ff1ccfd899e65a60f082c3fb7200d6fcac5
farahbakhsh3 <farahbakhsh3@gmail.com>
2023-11-29 18:13:17 +0330

    Step 0 : Project initiated...

---
+++ src/biginteger/BigInteger.java
@@ -0,0 +1,15 @@
+// An lagorithm for bigIntegerNumbers in java
+// Step 0 : build an structure for BigIntegers
+// Step 1 : Addition , Subtract
+package biginteger;
+
+public class BigInteger {
+
+    public static void main(String[] args) {
+        
+        String P1 = "12345678901234567890";
+        String P2 = "98765432109876543210";
+        
+        // And Continue...
+    }    
+}
