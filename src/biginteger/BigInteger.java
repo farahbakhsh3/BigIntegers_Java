d53cff939fdd36c6c6a4bad2adb9160df98eafbb
farahbakhsh3 <farahbakhsh3@gmail.com>
2023-12-08 15:44:01 +0330

    class BigIntegers implemented. method Add implemented. negative sign implemented.

--- src/biginteger/BigInteger.java
+++ src/biginteger/BigInteger.java
@@ -2,14 +2,26 @@
 // Step 0 : build an structure for BigIntegers
 // Step 1 : Addition , Subtract
 package biginteger;
+import biginteger.BigIntegers;
+        
 
 public class BigInteger {
 
     public static void main(String[] args) {
         
-        String P1 = "12345678901234567890";
-        String P2 = "98765432109876543210";
+        String P1 = "+100001";
+        String P2 = "123321";
         
         // And Continue...
+        BigIntegers p1 = new BigIntegers(P1);
+        System.out.println(p1.toString());        
+        BigIntegers p2 = new BigIntegers(P2);
+        System.out.println(p2.toString());       
+        
+        //p1.toStringIndex();
+        //p2.toStringIndex();
+
+        p1.Add(p2);
+        System.out.println(p1.toString());        
     }    
 }
