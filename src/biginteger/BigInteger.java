6a820518a749e790c2e40fdb3e49b127ff02fda7
farahbakhsh3 <farahbakhsh3@gmail.com>
2023-12-09 07:44:39 +0330

    Near to final. Addition and Subtract is compleated. Negative and Positive numbers is included. ----- Some minor changes is needed. zerros in the begining of a number need to deleted. :: ->  00012 should be convert to --> 12 -----

--- src/biginteger/BigInteger.java
+++ src/biginteger/BigInteger.java
@@ -1,27 +1,31 @@
-// An lagorithm for bigIntegerNumbers in java
-// Step 0 : build an structure for BigIntegers
-// Step 1 : Addition , Subtract
+
 package biginteger;
+
 import biginteger.BigIntegers;
-        
+
 
 public class BigInteger {
 
     public static void main(String[] args) {
         
-        String P1 = "+100001";
-        String P2 = "123321";
-        
+        String P1 = "20";
+        String P2 = "25";
+
         // And Continue...
         BigIntegers p1 = new BigIntegers(P1);
-        System.out.println(p1.toString());        
+        System.out.println(p1.toString());
+
         BigIntegers p2 = new BigIntegers(P2);
-        System.out.println(p2.toString());       
-        
-        //p1.toStringIndex();
-        //p2.toStringIndex();
+        System.out.println(p2.toString());
 
         p1.Add(p2);
-        System.out.println(p1.toString());        
-    }    
+        System.out.println(p1.toString());
+        System.err.println("---------------");
+
+        p1.Sub(p2);
+        System.out.println(p1.toString());
+        System.err.println("---------------");
+
+        //System.err.println(p1.compareTo(p2));
+    }
 }
--- src/biginteger/BigIntegers.java
+++ src/biginteger/BigIntegers.java
@@ -1,11 +1,15 @@
 package biginteger;
 
-import jdk.internal.misc.Signal;
-
 public class BigIntegers {
 
     private byte[] number_array;
     private boolean isNegative = false;
+
+    public enum compResultEnum {
+        bothIsEqual,
+        thisIsGreater,
+        thisIssmaller,
+    }
 
     public BigIntegers(String number) {
         Boolean haveSign = false;
@@ -24,12 +28,11 @@ public class BigIntegers {
         this.number_array = new byte[ub - lb];
         for (int i = lb; i < ub; i++) {
             char num = number.charAt(i);
-            if (num > '0' && num < '9') {
+            if (num >= '0' && num <= '9') {
                 this.number_array[i - lb] = Byte.parseByte(Character.toString(num));
             } else {
-                
+                throw new IllegalArgumentException("Format is not correct.");
             }
-
         }
     }
 
@@ -43,21 +46,63 @@ public class BigIntegers {
         return x;
     }
 
-    public void toStringIndex() {
-        int l = this.number_array.length;
-        System.out.println();
-        for (int i = 0; i < l; i++) {
-            System.out.printf("%d : %d\n", i, this.number_array[i]);
+    public compResultEnum compareTo(BigIntegers p2) {
+        int p1_l = this.number_array.length;
+        int p2_l = p2.number_array.length;
+
+        if (this.isNegative && !p2.isNegative) {
+            return compResultEnum.thisIssmaller;
         }
+
+        if (!this.isNegative && p2.isNegative) {
+            return compResultEnum.thisIsGreater;
+        }
+
+        if (p1_l > p2_l) {
+            if (this.isNegative) {
+                return compResultEnum.thisIssmaller;
+            } else {
+                return compResultEnum.thisIsGreater;
+            }
+        }
+
+        if (p2_l > p1_l) {
+            if (p2.isNegative) {
+                return compResultEnum.thisIsGreater;
+            } else {
+                return compResultEnum.thisIssmaller;
+            }
+        }
+
+        for (int idx = 0; idx < p1_l; idx++) {
+            if (this.number_array[idx] > p2.number_array[idx]) {
+                if (this.isNegative) {
+                    return compResultEnum.thisIssmaller;
+                } else {
+                    return compResultEnum.thisIsGreater;
+                }
+            } else if (this.number_array[idx] < p2.number_array[idx]) {
+                if (p2.isNegative) {
+                    return compResultEnum.thisIsGreater;
+                } else {
+                    return compResultEnum.thisIssmaller;
+                }
+            }
+        }
+        return compResultEnum.bothIsEqual;
     }
 
-    public void Add(BigIntegers p2) {
+    public compResultEnum compareTo(String p2) {
+        BigIntegers bi_p2 = new BigIntegers(p2);
+        return this.compareTo(bi_p2);
+    }
+
+    private BigIntegers Addition(BigIntegers p2) {
         int l1 = this.number_array.length;
         int l2 = p2.number_array.length;
 
-        int l = l1 > l2 ? l1 : l2;
         String sum = "";
-        int s = 0;
+        int s;
         int c = 0;
         for (int idx1 = l1 - 1, idx2 = l2 - 1;
                 idx1 >= 0 || idx2 >= 0;
@@ -83,11 +128,102 @@ public class BigIntegers {
         }
 
         BigIntegers out = new BigIntegers(sum);
-        this.number_array = out.number_array;
+        out.isNegative = false;
+        return out;
     }
 
-    public void Add(String p2) {
-        BigIntegers bi_p2 = new BigIntegers(p2);
-        this.Add(bi_p2);
+    public void Add(BigIntegers p2) {
+        BigIntegers x;
+        //+A + +B -> A+B -> A.Addition(B)
+        if (!this.isNegative && !p2.isNegative) {
+            x = this.Addition(p2);
+        } //+A + -B -> A-B -> A.Subtract(B)
+        else if (!this.isNegative && p2.isNegative) {
+            p2.isNegative = false;
+            x = this.Subtract(p2);
+            p2.isNegative = true;
+        } //-A + +B -> -A+B -> B-A -> B.Subtract(A)
+        else if (this.isNegative && !p2.isNegative) {
+            this.isNegative = false;
+            x = p2.Subtract(this);
+        } //-A + -B -> -A-B -> -(A+B) -> -(A.Addition(B))
+        else //(this.isNegative && p2.isNegative) 
+        {
+            p2.isNegative = false;
+            this.isNegative = false;
+            x = this.Addition(p2);
+            p2.isNegative = true;
+            x.isNegative = true;
+        }
+        this.number_array = x.number_array;
+        this.isNegative = x.isNegative;
+    }
+
+    private BigIntegers Subtract(BigIntegers p2) {
+        if (this.compareTo(p2) == compResultEnum.bothIsEqual) {
+            return new BigIntegers("0");
+        } else if (this.compareTo(p2) == compResultEnum.thisIssmaller) {
+            BigIntegers out = p2.Subtract(this);
+            out.isNegative = true;
+            return out;
+        } //else {
+
+        int l1 = this.number_array.length;
+        int l2 = p2.number_array.length;
+
+        String sub = "";
+        int s;
+        int c = 0;
+        for (int idx1 = l1 - 1, idx2 = l2 - 1;
+                idx1 >= 0 || idx2 >= 0;
+                idx1--, idx2--) {
+            s = 0;
+            s += c;
+            if (idx1 >= 0) {
+                s += this.number_array[idx1];
+            }
+            if (idx2 >= 0) {
+                s -= p2.number_array[idx2];
+            }
+            if (s < 0) {
+                s += 10;
+                c = -1;
+            } else {
+                c = 0;
+            }
+            sub = s + sub;
+        }
+        if (c == -1) {
+            System.err.println("c: "+c);
+        }
+        return new BigIntegers(sub);
+    }
+
+    public void Sub(BigIntegers p2) {
+        BigIntegers x;
+        //+A - +B -> A-B -> A.Subtract(B)
+        if (!this.isNegative && !p2.isNegative) {
+            x = this.Subtract(p2);
+        } //+A - -B -> A+B -> A.Addition(B)
+        else if (!this.isNegative && p2.isNegative) {
+            p2.isNegative = false;
+            x = this.Addition(p2);
+            p2.isNegative = true;
+        } //-A - +B -> -A - +B -> -A-B -> -(A+B) -> -(A.Addition(B))
+        else if (this.isNegative && !p2.isNegative) {
+            this.isNegative = false;
+            x = this.Addition(p2);
+            x.isNegative = true;
+        } //-A - -B -> -A+B -> B-A -> B.Subtract(A)
+        else //(this.isNegative && p2.isNegative) 
+        {
+            p2.isNegative = false;
+            this.isNegative = false;
+            x = p2.Subtract(this);
+            p2.isNegative = true;
+        }
+
+        this.number_array = x.number_array;
+        this.isNegative = x.isNegative;
     }
 }
