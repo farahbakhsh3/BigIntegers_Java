0db1d192bdff3087d64f50b815ae70ee9e6dd4d7
m-m-kh-py <khademim217@gmail.com>
2023-12-27 13:47:46 +0330

    'M_M_KH'

---
+++ src/MMKH/Test.java
@@ -0,0 +1,12 @@
+package MMKH;
+
+import MMKH.BigInteger;
+
+public class Test {
+
+    public static void main(String[] args) {
+        BigInteger n1 = new BigInteger("79783241");
+        BigInteger n2 = new BigInteger("543631");
+        System.out.println(n1.sub(n2));
+    }
+}
